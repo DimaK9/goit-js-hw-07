23e8a3e069efbdbb81a61409d09c8a6fbba95e5e
DimaK9 <103372116+DimaK9@users.noreply.github.com>
2022-10-27 22:53:49 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryContainerEl = document.querySelector('.gallery');
+
+const galleryMarkup = createGallery(galleryItems)
+galleryContainerEl.insertAdjacentHTML('beforeend',galleryMarkup);
+
+function createGallery(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return  `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+</a>
+</div>
+`
+    })
+.join('');
+}
+
+console.log(galleryContainerEl);
+
+
+
+
