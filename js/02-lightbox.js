f197bdcfbbe93bfe47e1792ad33c53910dfa4c71
DimaK9 <103372116+DimaK9@users.noreply.github.com>
2022-10-30 20:10:36 +0200

    виправив друге завдання. Наче працює

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,49 +3,23 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryContainerEl = document.querySelector('.gallery');
 
-const galleryMarkup = createGallery(galleryItems)
-
-function createGallery(galleryItems) {
-    return galleryItems
-    .map(({preview, original, description}) => {
-        return  `
-        <a class="gallery__item" href="${original}">
+function createGalleryItems({ preview, original, description }) {
+  return galleryContainerEl.insertAdjacentHTML(
+    'beforeend',
+    `
+      <a class="gallery__item" href="${original}">
         <img class="gallery__image"
           src="${preview}"
           alt="${description}"
         />
       </a>
-`
-    })
-.join('');
-}
+    `
+  )
+};
 
-galleryContainerEl.insertAdjacentHTML('beforeend',galleryMarkup);
+galleryItems.map(createGalleryItems).join("");
 
-console.log(galleryContainerEl);
-
-const ligthBox = new SimpleLightbox(".galleryContainerEl a", {
+new SimpleLightbox(".gallery a", {
     captionsData: "alt",
     captionDelay: 250,
   });
-
-// const gallery = document.querySelector(".gallery");
-// const markup = galleryItems
-//   .map((item) => {
-//     return `<a class = "gallery__link" href = "${item.original}">
-//     <img
-//  class="gallery__image"
-//  src="${item.preview}"
-// title="${item.description}"
-// alt="${item.description}"
-// width="900" height="650"
-// />
-//  </a>`;
-//   })
-//   .join("");
-
-// gallery.insertAdjacentHTML("beforeend", markup);
-
-// const ligthBox = new SimpleLightbox(".gallery a", {
-//   captionDelay: 250,
-// });
